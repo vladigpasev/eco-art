fd4c1aa6d8a0bf17818f4a45d26ea2d21e5d7078
Vladimir Pasev <vladimir.pasev@lazykey.com>
2023-09-12 10:22:09 +0300

    Middleware ignore sign in and sign up page

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { NextMiddleware } from "next/server";
 const p: NextMiddleware = (params, event) => {
   console.log(params.url);
   // Добавяме допълнителна проверка за новия път /api/events/get
-  if (!params.url.endsWith('/api/webhooks/clerk') && !params.url.endsWith('/api/events/get')) {
+  if (!params.url.endsWith('/api/webhooks/clerk') && !params.url.endsWith('/api/events/get') && !params.url.endsWith('/sign-in') && !params.url.endsWith('/sign-up')) {
     return authMiddleware({})(params, event);
   }
   
